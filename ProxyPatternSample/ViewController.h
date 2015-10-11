55b244766634d73ea6df92fe0bee57b2c4374b65
chengkaizone <chengkaizone@163.com>
2015-10-11 15:47:22 +0800

    Initial Commit

---
+++ ProxyPatternSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProxyPatternSample
+//
+//  Created by lance on 15/10/11.
+//  Copyright © 2015年 lance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
